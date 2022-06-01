a8847ac3307f50afc1daf208c675ef12a9dbb8fd
tomv2 <89455845+tomv2@users.noreply.github.com>
2022-06-01 11:49:14 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,21 +2,7 @@ import can
 import os
 
 print('Initialising can bus...')
-
-def initialiseCan():
-    try:
-        os.system("sudo /sbin/ip link set can0 up type can bitrate 500000") #check ECU bitrate is 500000
-        print('Connected')
-    except OSError:
-        print('Cannot find PiCAN board')
-        print('\nAttempting again...')
-        os.system("sudo /sbin/ip link set can0 down")
-        print('Will attempt again in 30 seconds')
-        print('Have ran link down command')
-        time.sleep(30)
-        initialiseCan()
-
-initialiseCan()
+os.system("sudo /sbin/ip link set can0 up type can bitrate 500000") #check ECU bitrate is 500000
 print('System ready')
 bus = can.interface.Bus(channel='can0', bustype='socketcan')
 notifier = can.Notifier(bus, [can.Printer()])
