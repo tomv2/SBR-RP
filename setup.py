f3f462f1e2efc2656d332fe99e9fbaef94f5448e
tomv2 <89455845+tomv2@users.noreply.github.com>
2022-06-01 11:52:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,4 +7,4 @@ os.system("sudo /sbin/ip link set can0 up type can bitrate 500000") #check ECU b
 print('System ready')
 bus = can.interface.Bus(channel='can0', bustype='socketcan')
 notifier = can.Notifier(bus, [can.Printer()])
-time.sleep(10000)
+time.sleep(10)
