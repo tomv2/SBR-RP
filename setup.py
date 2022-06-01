793499b0002905f6fca46da10a87905f99f7ef9d
tomv2 <89455845+tomv2@users.noreply.github.com>
2022-06-01 15:32:46 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ import time
 print('Initialising can bus...')
 os.system("sudo /sbin/ip link set can0 up type can bitrate 500000") #check ECU bitrate is 500000
 print('System ready')
-python -m can.logger -c can0 -i socketcan -b 500000 -f log1.log
+os.system("python -m can.logger -c can0 -i socketcan -b 500000 -f log1.log")
 while True:
   time.sleep(0.1)
