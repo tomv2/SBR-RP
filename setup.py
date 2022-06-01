4f3f0a873974f1cba142a938800c0d5dccef502c
tomv2 <89455845+tomv2@users.noreply.github.com>
2022-06-01 15:30:34 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ import time
 print('Initialising can bus...')
 os.system("sudo /sbin/ip link set can0 up type can bitrate 500000") #check ECU bitrate is 500000
 print('System ready')
-bus = can.interface.Bus(channel='can0', bustype='socketcan')
-notifier = can.Notifier(bus, [can.Printer()])
+python -m can.logger -c can0 -i socketcan -b 500000 -f log1.log
 while True:
   time.sleep(0.1)
