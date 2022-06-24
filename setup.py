cb4428cb82d9ecdb66b6e4e0e2e03d2e57b1e6e2
tomv2 <89455845+tomv2@users.noreply.github.com>
2022-06-24 11:00:27 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ print('Initialising can bus...')
 os.system("sudo /sbin/ip link set can0 up type can bitrate 500000")
 print('System ready')
 os.system("python -m can.logger -c can0 -i socketcan -b 500000 -f dashboard.log")
-os.system("python Datalogger.py")
+os.system("python DataLogger.py")
 os.system("python main.py")
 while True:
   time.sleep(0.1)
