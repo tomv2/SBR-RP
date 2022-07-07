c0e1b294e6bc81d02e0a1bd3b2c2182ce38eaa8b
tomv2 <89455845+tomv2@users.noreply.github.com>
2022-07-07 08:21:52 +0100

    Update rpm.py

--- backend/rpm/rpm.py
+++ backend/rpm/rpm.py
@@ -8,41 +8,29 @@ def rpm():
          for line in f:
             n = line[33]+line[34]+line[35]+line[36]
             enginespeed = (int(n, 16))
-            if enginespeed > 500:
-               ff
-            if enginespeed > 1000:
-               ff
-            if enginespeed > 1500:
-               ff
-            if enginespeed > 2000:
-               ff
             if enginespeed > 2500:
-               ff
+               enginespeed2 = enginespeed * 1.0143541
             if enginespeed > 3000:
-               ff
+               enginespeed2 = enginespeed * 1.0287082
             if enginespeed > 3500:
-               ff
+               enginespeed2 = enginespeed * 1.0430623
             if enginespeed > 4000:
-               ff
+               enginespeed2 = enginespeed * 1.0574164
             if enginespeed > 4500:
-               ff
+               enginespeed2 = enginespeed * 1.0717705
             if enginespeed > 5000:
-               ff
+               enginespeed2 = enginespeed * 1.0861246
             if enginespeed > 5500:
-               ff
+               enginespeed2 = enginespeed * 1.1004787
             if enginespeed > 6000:
-               ff
-            if enginespeed > 6500:
-               ff
+               enginespeed2 = enginespeed * 1.1148328
             if enginespeed > 7000:
-               ff
+               enginespeed2 = enginespeed * 1.1291869
             if enginespeed > 7500:
-               ff
+               enginespeed2 = enginespeed * 1.143541
             if enginespeed > 8000:
-               ff
-            if enginespeed > 8500:
-               ff
-            if enginespeed > 9000:
                enginespeed2 = enginespeed * 1.157895
+            if enginespeed > 9000:
+               enginespeed2 = enginespeed * 
             eel.updateRPM(enginespeed2)
             time.sleep(0.1)
